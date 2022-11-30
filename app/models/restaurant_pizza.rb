f18f870367be9c1c9e802d9b7a8cb87b9d028ed5
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-11-30 12:23:06 -0800

    commit tree fa5d993211f0e139ac2c02eff79a2697045ee054

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -2,5 +2,5 @@ class RestaurantPizza < ApplicationRecord
   belongs_to :pizza
   belongs_to :restaurant
 
-  validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
+  validates :price, inclusion: { in: 1..30 }
 end
